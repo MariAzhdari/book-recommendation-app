bfd0079e54dc7ce86e7f2be97b0df1c016902143
Mari Azh <Marzi.aghchay@gmail.com>
2023-06-08 00:14:24 +0100

    front start

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -18,8 +17,6 @@ app.get("/", (req, res) => {
   res.json("hello");
 });
 
-
-//checked in localhost:8800/books
 app.get("/books", (req, res) => {
   const q = "SELECT * FROM books";
   db.query(q, (err, data) => {
@@ -31,8 +28,6 @@ app.get("/books", (req, res) => {
   });
 });
 
-
-//I checked this part with postman
 app.post("/books", (req, res) => {
   const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
 
@@ -49,6 +44,33 @@ app.post("/books", (req, res) => {
   });
 });
 
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = " DELETE FROM books WHERE id = ? ";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q =
+    "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
 
 app.listen(8800, () => {
   console.log("Connected to backend.");
