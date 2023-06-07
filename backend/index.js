5d122636ae2152343fb309903da1b4b365f3124a
Mari Azh <Marzi.aghchay@gmail.com>
2023-06-07 23:02:42 +0100

    get and post

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,48 @@ app.use(cors());
 app.use(express.json());
 
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Mars@1994",
+  database: "test",
+});
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+
+//checked in localhost:8800/books
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+
+//I checked this part with postman
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
 
 app.listen(8800, () => {
   console.log("Connected to backend.");
